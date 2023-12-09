e15e85bd46941dd645d4aceb66ab394346113a4e
kaptast <kaptas.tamas@gmail.com>
2023-12-10 00:49:26 +0100

    set the color theme and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ export default {
   content: ['./src/**/*.{html|js|ts|svelte'],
   theme: {
     extend: {},
+    colors: {
+      'text': '#f0f0f0',
+      'background': '#000000',
+      'primary': '#a9eac4',
+      'secondary': '#1b2720',
+      'accent': '#ff3d40',
+     },
+     fontFamily: {
+       poppins: ['Poppins', 'sans-serif']
+     }
   },
   plugins: [],
 }
