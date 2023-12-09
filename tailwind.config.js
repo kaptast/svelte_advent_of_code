98bb466cd2767ce03d44f5b951f36db9ce68a194
kaptast <kaptas.tamas@gmail.com>
2023-12-10 00:55:20 +0100

    prettier format

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,15 @@ export default {
   theme: {
     extend: {},
     colors: {
-      'text': '#f0f0f0',
-      'background': '#000000',
-      'primary': '#a9eac4',
-      'secondary': '#1b2720',
-      'accent': '#ff3d40',
-     },
-     fontFamily: {
-       poppins: ['Poppins', 'sans-serif']
-     }
+      text: '#f0f0f0',
+      background: '#000000',
+      primary: '#a9eac4',
+      secondary: '#1b2720',
+      accent: '#ff3d40'
+    },
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif']
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
